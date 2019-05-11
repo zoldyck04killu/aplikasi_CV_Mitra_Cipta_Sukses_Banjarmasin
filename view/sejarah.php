08bd5b6235484a6b4adeff8b9c52f7333ba3f84e
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2019-05-12 00:41:30 +0700

    visi misi, sejarah

--- view/sejarah.php
+++ view/sejarah.php
@@ -1,9 +1,28 @@
 <div class="card">
   <div class="card-header">
-    Sejarah
+    Sejarah CV Mitra Cipta Sukses
   </div>
   <div class="card-body">
-  
-
+    <p>
+      CV. Mitra Cipta Sukses di bentuk pada awal tahun 2017, yaitu salah satu perusahaan yang bergerak pada bidang jasa Digital Printing dan Percetakan di kota Banjarmasin.
+    </p>
+    <p>
+      Berawal dari perusahaan induknya yaitu CV. Marco Cipta Selaras yang bergerak di bagian kontruksi bangunan dan interior, CV Mitra Cipta Sukses lebih memfokuskan pada bagian Advertising yaitu di bagian Percetakan dan digital printing.
+    </p>
+    <p>
+      Seiring perkembangannya sekarang perusahaan ini tetap berdiri dengan melakukan berbagai inovasi dalam mempertahankan eksistensi percetakan digital yang saat ini sudah memiliki pesaing dalam bidang yang sama.
+    </p>
+    <p>
+      Terletak di Jl. R.K. ILIR No.563 Banjarmasin perusahaan percetakan digital ini memiliki moto perusahaan yaitu “Kreatif, inovatif, serta quality control ada di setiap pekerjaan kami”. Yang diharapkan sebagaimana moto tersebut dapat menjadikan motivasi bersama dalam mencapai tujuan yang diharapkan perusahaan.
+    </p>
+    <p>
+      Keberadaan salah satu perusahaan percetakan digital, CV. Mitra Cipta Sukses ini diharapkan menjadi sebuah wadah pengembangan wawasan dan potensi diri bagi karyawan dan solusi cetak cepat untuk semua kalangan masyarakat khususnya warga Banjarmasin.
+    </p>
+    <p>
+      Disamping itu pula demi mendorong roda perekonomian daerah telah menjadi tujuan perusahaan untuk memberikan keluasan lapangan pekerjaan untuk sumber daya manusia Kalimantan Selatan agar dapat membantu serta memperbaiki kemandirian ekonomi sosial.
+    </p>
+    <p>
+      Oleh sebab itu diharapkan dengan kehadiran perusahaan ini dapat mensinergikan semua kalangan masyarakat dalam memenuhi semua kebutuhan sosial. Dengan moto yang telah didengungkan perusahaan pula diharapkan perusahaan ini tetap menjadi yang terbaik di setiap benak masyarakat.
+    </p>
   </div>
 </div>
--- view/visi-misi.php
+++ view/visi-misi.php
@@ -1,8 +1,17 @@
 <div class="card">
   <div class="card-header">
-    Visi Misi
+    Visi dan Misi Perusahaan
   </div>
   <div class="card-body">
-
+    <ul>
+      <li>
+        Visi <br>
+        Membentuk Perusahaan jasa percetakan dengan melakukan penanaman modal untuk memberi dan mewujudkan peningkatan kehidupan dan kesejahteraan masyarakat dan ikut mncerdaskan kehidupan bangsa terutama di daerah domisili perusahaan dengan didasarkan Pancasila.
+      </li>
+      <li>
+        Misi <br>
+        Menyelenggarakan pelayanan jasa cetak bagi masyarakat yang membutuhkan pelayanan dengan prinsip pelayanan yang memuaskan dalam hal kualitas/mutu, tepat waktu, pelayanan simpatik penuh tanggung jawab dan dengan harga yang wajar sehingga perusahaan dapat berkembang maju serta dapat mensejahterakan seluruh pihak yang berkepentingan baik pelanggan, karyawan, pemilik, mitra kerja dan masyarakat di lingkungan perusahaan dengan dasar etika usaha yang bersih, baik dan benar.
+      </li>
+    </ul>
   </div>
 </div>
